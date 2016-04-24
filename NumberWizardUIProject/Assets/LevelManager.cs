2d55209f397e06cc669959c4d26bf0cb903ee9be
illbrb <illbrbgit@gmail.com>
2016-04-24 16:44:29 +0100

    update

---
+++ NumberWizardUIProject/Assets/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string levelName){
+		Application.LoadLevel(levelName);
+		Debug.Log("Level Load Requested for: " + levelName);
+		
+	}
+
+	public void QuitGame(){
+		Debug.Log("Quit Game Requested");
+		Application.Quit();		
+	}
+}
---
+++ NumberWizardUIProject/Assets/NumberWizard.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+	
+	public int maxGuessesAllowed = 10;
+	
+	public Text guessText;
+	
+	void Start () {
+		StartGame();
+	}
+	
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		guess = 500;
+		max = max + 1;
+	}
+	
+	public void GuessHigher()
+	{
+		min = guess;
+		NextGuess();
+	}
+	
+	public void GuessLower()
+	{
+		max = guess;
+		NextGuess();
+	}
+	
+	void NextGuess () {
+		guess = (max + min) / 2;
+		guessText.text = guess.ToString();
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		if (maxGuessesAllowed <= 0)
+		{
+			Application.LoadLevel("Win");
+		}
+	}
+}
