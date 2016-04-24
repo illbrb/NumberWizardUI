34cc512dea9c51ba9a115cf33177a4efacfd4c9d
illbrb <illbrbgit@gmail.com>
2016-04-24 16:51:25 +0100

    update

--- NumberWizardUIProject/Assets/LevelManager.cs
+++ NumberWizardUIProject/Assets/LevelManager.cs
@@ -4,13 +4,10 @@ using System.Collections;
 public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string levelName){
-		Application.LoadLevel(levelName);
-		Debug.Log("Level Load Requested for: " + levelName);
-		
+		Application.LoadLevel(levelName);		
 	}
 
 	public void QuitGame(){
-		Debug.Log("Quit Game Requested");
 		Application.Quit();		
 	}
 }
--- NumberWizardUIProject/Assets/NumberWizard.cs
+++ NumberWizardUIProject/Assets/NumberWizard.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using LevelManager;
 
 public class NumberWizard : MonoBehaviour {
 	// Use this for initialization
